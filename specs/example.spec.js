8f555ef0ff1e63561a5a663bfa72cacf6db7b852
Iurii Lopatenko <52218759+ILopatenko@users.noreply.github.com>
2022-02-05 21:19:27 -0800

    Added Mocha and Chai. First test was runned successfully (#2)

---
+++ specs/example.spec.js
@@ -0,0 +1,10 @@
+import { expect } from 'chai';
+
+describe('Math functions', () => {
+  it('A + B = C', () => {
+    const a = 4;
+    const b = 7;
+    const c = a + b;
+    expect(c).to.eq(11);
+  });
+});
