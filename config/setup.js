a62c7f3c3ece3a4c296ee4f10591d5a97ba06ce1
Iurii Lopatenko <52218759+ILopatenko@users.noreply.github.com>
2022-02-15 16:46:38 -0800

    Api helpers (#12)
    * AuthHelper class
    
    login method. fixed documentation

--- config/setup.js
+++ config/setup.js
@@ -1,16 +1,8 @@
 import 'dotenv/config';
-import supertest from 'supertest';
+import AuthHelper from '../helpers/auth.helper';
 
 before(async () => {
-  //Create a request object
-  const request = await supertest(process.env.BASE_URL);
-  await request
-    //Setup a request method - POST and an endpoint - /auth
-    .post('/auth')
-    //Setup payload - object with 2 keys - login and password (and their values)
-    .send({ login: process.env.LOGIN, password: process.env.PASSWORD })
-    //Save a response from server to result variable
-    .then((res) => {
-      process.env['TOKEN'] = res.body.token;
-    });
+  const authHelper = new AuthHelper();
+  await authHelper.login(process.env.LOGIN, process.env.PASSWORD);
+  process.env['TOKEN'] = authHelper.response.body.token;
 });
--- specs/auth.spec.js
+++ specs/auth.spec.js
@@ -1,51 +1,35 @@
 import { expect } from 'chai';
-import supertest from 'supertest';
+import AuthHelper from '../helpers/auth.helper';
 //This describe is a main test suite
 describe('Auth test suite', () => {
-  //Create a request object
-  const request = supertest(process.env.BASE_URL);
-  //Create a new variable to store a response from server
-  let result;
+  //Create a new instance of AuthHelper
+  let authHelper = new AuthHelper();
   describe('Successful login sub suite (happy path, positive tests)', () => {
     //BEFORE hook - will be runned 1st (before all other suits/tests)
     before(async () => {
-      //Send async request
-      await request
-        //Setup a request method - POST and an endpoint - /auth
-        .post('/auth')
-        //Setup payload - object with 2 keys - login and password (and their values)
-        .send({ login: process.env.LOGIN, password: process.env.PASSWORD })
-        //Save a response from server to result variable
-        .then((res) => {
-          result = res;
-        });
+      //Send async request using our HELPER method login
+      await authHelper.login(process.env.LOGIN, process.env.PASSWORD);
     });
     it('Checking that response status code is 200', () => {
-      expect(result.statusCode).to.eq(200);
+      expect(authHelper.response.statusCode).to.eq(200);
     });
     it('Checking that response contains an authorization token', () => {
-      expect(result.body.token).not.to.be.undefined;
+      expect(authHelper.response.body.token).not.to.be.undefined;
     });
   });
   describe('Unsuccessful login sub suite (unhappy path, negative tests)', () => {
     //BEFORE hook - will be runned 1st (before all other suits/tests)
     before(async () => {
-      //Send async request
-      await request
-        //Setup a request method - POST and an endpoint - /auth
-        .post('/auth')
-        //Setup payload - object with 2 keys - login and password (and their values)
-        .send({ login: 'tralala', password: 'trulala' })
-        //Save a response from server to result variable
-        .then((res) => {
-          result = res;
-        });
+      //Send async request using our HELPER method login
+      await authHelper.login('trulala', 'tralala');
     });
     it('Checking that response status code is 404', () => {
-      expect(result.statusCode).to.eq(404);
+      expect(authHelper.response.statusCode).to.eq(404);
     });
     it('Checking that error message is "Wrong login or password."', () => {
-      expect(result.body.message).to.eq('Wrong login or password.');
+      expect(authHelper.response.body.message).to.eq(
+        'Wrong login or password.'
+      );
     });
   });
 });
